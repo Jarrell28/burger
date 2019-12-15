040fec90724d41e573039ed85613ede6b2970d15
Jarrell Houston <jhouston2882@yahoo.com>
2019-12-14 19:07:01 -0600

    set up controller functions

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
