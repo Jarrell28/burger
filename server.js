c85315258151fef49be3729c133dd3f5d0a235f9
Jarrell Houston <jhouston2882@yahoo.com>
2019-12-14 19:28:20 -0600

    completed overall setup

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // // Import routes and give the server access to them.
-// var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgers_controllers.js");
 
-// app.use(routes);
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
